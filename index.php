16bc53802ed9709afd134e87419c3b5efa99cc5a
GGive26 <precieudieudonne@gmail>
2023-09-27 14:45:42 -0400

    test

--- index.php
+++ index.php
@@ -1,6 +1,15 @@
-<h1>Mon gros titre</h1>
+<h1>Mon Titre</h1>
 <?php 
-echo"Mon gros titre";
-
+echo"<h2>Mes informations personnel</h2><br>";
+$Prenom="Precieu";
+$nom="KOSSO";
+$age=21;
+$villeNaissance="Brazzaville";
+$paysNaissance="Congo";
+echo"<ul><li>Prenom: $Prenom</li></ul>";
+echo"<ul><li>Nom: $nom</li></ul>";
+echo"<ul><li>Age: $age ans</li></ul>";
+echo"<ul><li>Ville de Naissance: $villeNaissance</li></ul>";
+echo"<ul><li>Pays de Naissance: $paysNaissance</li></ul>";
 
 ?>
